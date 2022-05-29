f869a490b3d0d839f09ecb3b532b36384c5199d2
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2022-05-29 13:06:25 +0100

    Build 0.0.1
    Performed test build

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,35 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Development,
+    }
+
+    public enum Platform
+    {
+        None,
+        WebGL,
+    }
+
+    public enum Architecture
+    {
+        None,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637894230414333110);
+    public const string version = "0.0.1";
+    public const ReleaseType releaseType = ReleaseType.Development;
+    public const Platform platform = Platform.WebGL;
+    public const Architecture architecture = Architecture.WebGL;
+    public const Distribution distribution = Distribution.None;
+}
+
